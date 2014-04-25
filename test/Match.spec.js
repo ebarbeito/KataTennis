fda2232ec3e9e1aed89b0125e856ff939c14f8fe
Enrique Barbeito García <ebarbeito@gmail.com>
2014-04-25 19:03:12 +0200

    red: a match must have two players defined by user

--- test/Match.spec.js
+++ test/Match.spec.js
@@ -19,4 +19,10 @@ describe('Match', function() {
     expect(match.players.length).toBe(2);
   });
   
+  it('a match must have two players defined by user', function() {
+    var match = new Match('Nadal', 'Pete Sampras');
+    expect(match.players[0]).toBe('Nadal');
+    expect(match.players[1]).toBe('Pete Sampras');
+  });
+  
 });
