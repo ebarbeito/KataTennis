bbc207b474b05c7db30437fc06aa166222f82d1c
Enrique Barbeito García <ebarbeito@gmail.com>
2014-04-25 19:23:20 +0200

    test refactor and adding some context

--- test/Match.spec.js
+++ test/Match.spec.js
@@ -2,25 +2,29 @@
 
 var Match = require('../src/Match').Match;
 
-describe('Match', function() {
+describe('When a tennis match starts', function() {
+  
+  var anonymousMatch;
+  var match;
+  
+  beforeEach(function() {
+    anonymousMatch = new Match();
+    match = new Match('Nadal', 'Pete Sampras');
+  });
   
   it('can create a new tennis match', function() {
-    var match = new Match();
-    expect(match).toBeDefined();
+    expect(anonymousMatch).toBeDefined();
   });
   
   it('a match must have players', function() {
-    var match = new Match();
-    expect(match.players).toBeDefined();
+    expect(anonymousMatch.players).toBeDefined();
   });
   
   it('a match must have two players', function() {
-    var match = new Match();
-    expect(match.players.length).toBe(2);
+    expect(anonymousMatch.players.length).toBe(2);
   });
   
   it('a match must have two players defined by user', function() {
-    var match = new Match('Nadal', 'Pete Sampras');
     match.players.map(function(pj, i) {
       expect(match.players[i]).toBe(pj);
     });
